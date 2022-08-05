47c5e424cc4514a8d92de2d7af3d9ec8df4771d9
shahajalalweb <shahajalalbadsha5@gmail.com>
2022-08-05 09:26:36 +0600

    custom-hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import style from "../style/home.module.css"
+import useFetch from './useFetch'
 
-const loadingMessage = <p>Api data is Loading ....</p>
+
 
 const Home = () => {
-  const [todos, setTodos] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  
-  useEffect(() => {
 
-    fetch("https://jsonplaceholder.typicode.com/users/1/todos")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("fecthing is not successful");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setTodos(data);
-        setIsLoading(false);
-        setError(null);
-      })
-      .catch((error) => {
-        setError(error.message);
-        setIsLoading(false);
-      });
-
- 
-  }, []);
+  const {data, isLoading, error} = useFetch("https://jsonplaceholder.typicode.com/todos");
 
 
-  const todoElements = todos && todos.map((todo) => {
+
+
+  const loadingMessage = <p>Api data is Loading ....</p>
+
+  const todoElements = data && data.map((todo) => {
     return <p key={todo.id}> { todo.title}</p>
   })
 
