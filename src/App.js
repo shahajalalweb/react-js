67b33691eefc077cb0bbb75b0ec5453addc23781
shahajalalweb <shahajalalbadsha5@gmail.com>
2022-08-06 07:51:54 +0600

    update api heading

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-      <h1 className='h1'>Normal Counter</h1>
+      <h1 className='h1'>Api Data </h1>
       <Home />
     </div>
   );
