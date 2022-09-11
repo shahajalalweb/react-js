4302067c26fd046a7215966fd3ae580368a22180
shahajalalweb <shahajalalbadsha5@gmail.com>
2022-09-11 09:47:47 +0600

    useContext propDrilling

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useState } from 'react'
 import Component2 from './Component2'
+import { UserContext } from './useContext'
 
 export const Component1 = () => {
 
-    const [user, setUser] = useState({id: "11", name: "Badsha"})
+    const [user, setUser] = useState({id: "22", name: "Shahajalal Badsha"});
+    const [text, setText] = useState("PropDilling successfull");
 
   return (
-    <div>
-        <Component2  user={user}/>
-    </div>
+    <UserContext.Provider value={{user, text}}>
+        <Component2/>
+    </UserContext.Provider>
   )
 }
 
--- src/components/Component4.js
+++ src/components/Component4.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { UserContext } from './useContext'
 
-export const Component4 = ({user}) => {
+
+export const Component4 = () => {
+  
+  const {user, text} = useContext(UserContext);
+
+
   return (
     <div>
         <h2>{user.id}</h2>
         <h3>{user.name}</h3>
+        <p>{text}</p>
     </div>
   )
 }
