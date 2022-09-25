0af845d4d88514917432e79f916bd3df31151a88
shahajalalweb <shahajalalbadsha5@gmail.com>
2022-09-25 22:16:45 +0600

    crud oparation added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,65 @@
-import './App.css';
-import Home from './components/Home';
+import React, {useState, useEffect} from "react"
+// import style from "../style/home.module.css"
+
+const URL = "https://rest-api-without-db.herokuapp.com/users/"
+
+const App = () => {
+
+  const [users, setUsers] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getAllUsers = () => {
+    fetch(URL)
+    .then((res) => {
+      if(!res.ok) {
+       throw Error("could not fetch"); 
+      }
+      return res.json();
+    })
+
+    .then((data) => {
+      setUsers(data.users);
+    })
+
+    .catch((err) => {
+      setError(err.message);
+    })
+
+    .finally(() => {
+      setIsLoading(false);
+    })
+
+  }
+
+  useEffect(() => {
+    getAllUsers();
+  }, [])
 
 
 
-function App() {
+
   return (
-    <div className="App">
-      <h1 className='h1'>Normal Counter</h1>
-      <Home />
+    <div> 
+      {isLoading && <h2>Loading...</h2>}
+      {error && <h2>{error}</h2>}
+
+      {console.log(users)} 
+      {users &&
+          users.map((user) => {
+            const { id, username, email } = user;
+            return (
+              <article key={id}>
+                <p>{username}</p>
+                <p>{email}</p>
+
+              </article>
+            );
+          })}
+      
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App 
